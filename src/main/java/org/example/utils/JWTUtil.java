ed54ec3176e7febbce3d79970fd994e86d3987a2
longnt08 <thanhlong0j@gmail.com>
2025-03-06 19:37:04 +0700

    fix login, register

--- src/main/java/org/example/utils/JWTUtil.java
+++ src/main/java/org/example/utils/JWTUtil.java
@@ -2,20 +2,21 @@ package org.example.utils;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Encoders;
 import org.example.model.User;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 
 import java.util.Date;
 
 public class JWTUtil {
-    private static final String SECRET_KEY = "your_secret_key";
-
     public static String generateToken(User user) {
         return Jwts.builder()
                 .setSubject(user.getUsername())
                 .claim("role", user.getRole())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 3600000)) // 1 giờ
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
                 .compact();
     }
 }
--- src/main/java/org/example/utils/MongoDBUtil.java
+++ src/main/java/org/example/utils/MongoDBUtil.java
@@ -6,7 +6,7 @@ import com.mongodb.client.MongoDatabase;
 
 public class MongoDBUtil {
     private static final String URI = "mongodb+srv://diepanhnguyen2807:nhom6@cluster0.d9lbu.mongodb.net/";
-    private static final String DATABASE_NAME = "financial_management";
+    private static final String DATABASE_NAME = "financial_management;";
 
     public static MongoDatabase getDatabase() {
         MongoClient mongoClient = MongoClients.create(URI);
